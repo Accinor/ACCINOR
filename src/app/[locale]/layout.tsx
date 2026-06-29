3f3c65d98948d2cb7eecccec4b9d7f8f7ad88560
Yassine <yassine@example.com>
2026-06-29 10:16:53 +0100

    Pass locale explicitly to getMessages() in layout

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -54,7 +54,7 @@ export default async function LocaleLayout({ children, params }: Props) {
     notFound()
   }
 
-  const messages = await getMessages()
+  const messages = await getMessages({ locale })
 
   return (
     <html
